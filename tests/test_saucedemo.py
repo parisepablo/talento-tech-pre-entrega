2f0bf3414ab807e85c5e0e84cfb185367c6b14fe
Pablo Parise <parisepablo@outlook.com.ar>
2026-05-18 20:39:00 -0300

    Added test regarding the display of the first catalog product

--- tests/test_saucedemo.py
+++ tests/test_saucedemo.py
@@ -1,6 +1,7 @@
 from utils.helpers import login
 from selenium.webdriver.common.by import By 
 
+## Login test cases
 def test_login_accepted_credentials(driver):
     login(driver, "standard_user", "secret_sauce")
     assert driver.current_url == "https://www.saucedemo.com/inventory.html"
@@ -9,10 +10,22 @@ def test_login_accepted_credentials(driver):
 
 
 def test_login_wrong_credentials(driver):
-    login(driver, "locked_user", "secret_sauce")
+    login(driver, "wrong_user", "wrong_password")
     assert driver.current_url == "https://www.saucedemo.com/"
 
 
 def test_login_empty_credentials(driver):
     login(driver, "", "")
     assert driver.current_url == "https://www.saucedemo.com/"
+
+
+## Catalog test cases
+def test_catalog_products_displayed(driver):
+    login(driver, "standard_user", "secret_sauce")
+    title = driver.find_element(By.CLASS_NAME, "title").text
+    assert title == "Products"
+    inventory_items = driver.find_elements(By.CSS_SELECTOR, "[data-test='inventory-item']")
+    assert len(inventory_items) > 0
+    assert inventory_items[0].find_element(By.CSS_SELECTOR, "[data-test='inventory-item-name']").text == "Sauce Labs Backpack"
+    assert inventory_items[0].find_element(By.CSS_SELECTOR, "[data-test='inventory-item-price']").text == "$29.99"
+    assert inventory_items[0].find_element(By.CSS_SELECTOR, "[data-test='add-to-cart-sauce-labs-backpack']").is_displayed()
