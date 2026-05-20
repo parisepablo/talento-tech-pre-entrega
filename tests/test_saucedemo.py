84968a92ba16e5c26d8c5246a314e6c0f0674e1a
Pablo Parise <parisepablo@outlook.com.ar>
2026-05-20 08:33:38 -0300

    #Added some comments

--- tests/test_saucedemo.py
+++ tests/test_saucedemo.py
@@ -25,9 +25,11 @@ def test_login_empty_credentials(driver):
 def test_catalog_products_displayed(driver):
     login(driver, "standard_user", "secret_sauce")
     title = driver.find_element(By.CLASS_NAME, "title").text
+    #Validate product page displays correctly
     assert title == "Products"
     inventory_items = driver.find_elements(By.CSS_SELECTOR, "[data-test='inventory-item']")
     assert len(inventory_items) > 0
+    #Validate first product
     assert inventory_items[0].find_element(By.CSS_SELECTOR, "[data-test='inventory-item-name']").text == "Sauce Labs Backpack"
     assert inventory_items[0].find_element(By.CSS_SELECTOR, "[data-test='inventory-item-price']").text == "$29.99"
     assert inventory_items[0].find_element(By.CSS_SELECTOR, "[data-test='add-to-cart-sauce-labs-backpack']").is_displayed()
